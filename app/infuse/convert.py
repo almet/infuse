63e33d4006fc11082d99e1188d0d908f501dde30
Alexis Metaireau <alexis@notmyidea.org>
2011-06-10 18:57:14 +0200

    Add a convert to convert raw data into useful data.

--- app/infuse/convert.py
+++ app/infuse/convert.py
@@ -10,3 +10,150 @@ information like "the user have watched this resource for XX seconds",
 The implementation is based on Map/Reduce functions that are directly executed on
 the mongodb server.
 """
+
+import datetime
+import db
+
+STARTERS = ["ready", "activate"]
+ENDERS = ["deactivate", "close"]
+
+class TemporaryView(object):
+    def __init__(self):
+        self.started = None
+        self.finished = None
+        self.event = None
+        self.parent = None
+
+    def duration(self):
+        """return the duration, in seconds"""
+        return (int(self.finished) - int(self.started)) / 1000
+
+    def reset(self):
+        self.__init__()
+
+    def save(self):
+        view = db.views.View()
+        view.location = self.event['location']
+        view.user = self.event['user']
+        view.url = self.event['url']
+        view.duration = self.duration()
+        view.timestamp = int(self.event['timestamp'])
+        
+        # compute the time of day and the day of week
+        dt = datetime.datetime.fromtimestamp(int(self.event['timestamp'])/1000)
+        view.weekday = dt.weekday()
+
+        if dt.hour <= 2:
+            daytime = "0-3"
+        elif dt.hour <= 4:
+            daytime = "3-5"
+        elif dt.hour <= 6:
+            daytime = "5-7"
+        elif dt.hour <= 8:
+            daytime = "7-9"
+        elif dt.hour <= 10:
+            daytime = "9-11"
+        elif dt.hour <= 12:
+            daytime = "11-13"
+        elif dt.hour <= 14:
+            daytime = "13-15"
+        elif dt.hour <= 16:
+            daytime = "15-17"
+        elif dt.hour <= 18:
+            daytime = "17-19"
+        elif dt.hour <= 20:
+            daytime = "19-21"
+        elif dt.hour <= 22:
+            daytime = "21-23"
+        else:
+            daytime = "23-24"
+
+        view.daytime = daytime
+        view.save()
+
+        # does a resource with this url exist ? if not, create one
+        res = db.Resource.get_or_create(self.event['url'])
+        if self.parent and self.parent not in res.parents:
+            res.parents.append(self.parent)
+        res.save()
+
+
+def extract_views():
+    """Extract information about the resources views.
+
+    Reads the information from the Events collection (the raw information taken 
+    directly from the browsers) and convert it into something usable in the views 
+    collection."""
+    def _mark_as_processed(event):
+        if event:
+            event['processed'] = True
+            event.save()
+    
+    
+    for id in db.events.distinct("tab_id"):
+        # get all the events relative to this tab
+        events = db.events.Event.find({"tab_id": id}).sort("timestamp")
+       
+        # init variables for the loop
+        view = TemporaryView()
+        previous_url = None
+
+        for event in events:
+            if event['type'] in STARTERS:
+                view.started = event['timestamp']
+                view.event = event
+
+            elif view.started and event['type'] in ENDERS:
+                view.finished = event['timestamp']
+
+            # manage the relation between resources
+            # Add the relation between the resources (different "ready" states 
+            # with different urls on the same tab)
+            if event['type'] == "ready":
+                if previous_url:
+                    # add information to the view
+                    view.parent = previous_url
+
+                previous_url = event['url']
+
+            if view.finished:
+                # save the event in the db
+                # print("the resource %s have been viewed for %s seconds, the timestamp is %s" % (view.event['url'], view.duration(), view.event['timestamp']))
+                view.save()
+                view.reset()
+
+                # mark the two events as processed
+                _mark_as_processed(view.event)
+                _mark_as_processed(event)
+
+
+def reset():
+    """Remove all the resources/views and mark all the events as not processed.
+
+    This function is mainly used for test purposes"""
+    for event in db.events.Event.find({'processed': True}):
+        event.processed = False
+        event.save()
+
+    db.resources.drop()
+    db.views.drop()
+
+
+def summarize():
+    """Summarize all the information and put them in a document containing information
+    about:
+
+        - The URL
+        - The user
+        - The number of views for this resource for this user
+        - The overall view duration
+        - The geolocations for those views
+        - The time (period of the day, day of the week)
+
+    This is only based on views. The content is not downloaded in this document but
+    rather in a resource document to proceed. (Which is done in the download 
+    module).
+    """
+
+reset()
+#extract_views()
--- app/infuse/db.py
+++ app/infuse/db.py
@@ -1,14 +1,17 @@
+import datetime
+
 from mongokit import Connection, Document
 from pymongo import binary
 
-from infuse.settings import *
+from settings import *
 
 connection = Connection(MONGODB_HOST, MONGODB_PORT)
 
 events = connection[MONGODB_DB].events
 tabs = connection[MONGODB_DB].tabs
 users = connection[MONGODB_DB].users
-contents = connection[MONGODB_DB].contents
+resources = connection[MONGODB_DB].resources
+views = connection[MONGODB_DB].views
 
 @connection.register
 class User(Document):
@@ -30,7 +33,8 @@ class Event(Document):
             'tab_id': unicode,
             'browser': unicode,
             'user': User,
-            'location': tuple
+            'location': list,
+            'processed': bool,
     }
 
     authorized_types = Document.authorized_types + [tuple]
@@ -49,11 +53,50 @@ class TabRelation(Document):
 
 
 @connection.register
-class Content(Document):
+class Resource(Document):
     """Represents a url and its content"""
     structure = {
-            'url': str,
-            'content': unicode
+            'url': unicode,
+            'content': unicode,
+            'parents': list,
+            'processed': bool,
+            'date': datetime.datetime,
+    }
+
+    authorized_types = Document.authorized_types + [str]
+    default_values = {'processed': False, 'parents': [], 
+            'date': datetime.datetime.now()}
+    use_dot_notation = True
+
+    @staticmethod
+    def get_or_create(url):
+        """Return a resource, create one if one with the specified url doesnt 
+        already exists.
+
+        The returned resource is not persisted in db when returned, so there is a
+        need to call save() on it after using it.
+
+        :param url: the url of the resource
+        """
+        resource = resources.Resource.one({'url': url})
+        if not resource:
+            resource = resources.Resource()
+            resource.url = url
+            resource.processed = False
+
+        return resource
+
+
+@connection.register
+class View(Document):
+    structure = {
+            'url': unicode, 
+            'user': User,
+            'duration': int, 
+            'location': list,
+            'timestamp': int,
+            'daytime': str,
+            'weekday': int,
     }
 
     authorized_types = Document.authorized_types + [str]
